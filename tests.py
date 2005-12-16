9759cd6ee92dac57237b63833592ba931d16a6d4
Benji York <benji@benjiyork.com>
2005-12-16 22:40:51 +0000

    port XXX fixes from 3.2 branch (revisions 40833 and 40829)

--- tests.py
+++ tests.py
@@ -11,7 +11,7 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-"""XXX short summary goes here.
+"""test resolution of dotted names
 
 $Id$
 """
