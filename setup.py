c74fe7c6c9680d032772a993c17eb4c1b50c73e6
Baiju Muthukadan <baiju.m.mail@gmail.com>
2007-03-09 11:49:38 +0000

    - Added Buildout support  - Version incremented to 3.4dev

--- setup.py
+++ setup.py
@@ -1,13 +1,37 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.documenttemplate package
+
+$Id$
+"""
+
 from setuptools import setup, find_packages
 
 setup(
     name="zope.dottedname",
-    version="0.1dev",
+    version="3.4dev",
+    url='http://svn.zope.org/zope.dottedname',
+    license='ZPL 2.1',
+    description='Zope dottedname',
+    author='Zope Corporation and Contributors',
+    author_email='zope3-dev@zope.org',
+
     packages=find_packages('src'),
     package_dir={'':'src'},
     namespace_packages=['zope'],
     include_package_data=True,
-    install_requirements = ['setuptools'],
+    install_requires = ['setuptools'],
     zip_safe = False
     )
 
