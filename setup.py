e381e9e9a83947f331b43c51eba35bef6935a640
Stephan Richter <stephan.richter@gmail.com>
2007-10-02 14:52:40 +0000

    Let's try a new release.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name="zope.dottedname",
-      version = '3.4.1',
+      version = '3.4.2',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       description='Resolver for Python dotted names.',
