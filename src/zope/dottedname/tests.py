1a2440c457c1661ed392af98d4baec299c7530fe
Marius Gedminas <marius@gedmin.as>
2013-02-01 21:07:15 +0000

    Made tests pass on Python 3.2, 3.3 and PyPy.
    No code changes were required, just some renormalizers to unify exception
    and some builtin object representations.
    
    Add tox.ini and MANIFEST.in.
    
    I can't figure out tox -e coverage: looks like it doesn't even notice the
    zope.dottedname.resolve module?  Maybe I need some magic incantations in a
    setup.cfg or something?  I need to grab Tres and have him teach me.

--- src/zope/dottedname/tests.py
+++ src/zope/dottedname/tests.py
@@ -16,11 +16,34 @@
 import unittest
 import doctest
 import os
+import re
+
+from zope.testing.renormalizing import RENormalizing
 
 README = os.path.abspath(os.path.join(os.path.dirname(__file__), 'README.txt'))
 FLAGS = doctest.REPORT_NDIFF | doctest.ELLIPSIS
 
 def test_suite():
+    checker = RENormalizing([
+        # datetime is a class in Python 3, but a type in Python 2
+        (re.compile("<class 'datetime\.datetime'>"),
+         "<type 'datetime.datetime'>"),
+        # Python 3.2 adds quotes around the module name
+        (re.compile("ImportError: No module named '([^']*)'"),
+         r"ImportError: No module named \1"),
+        # and improves the message in other ways too
+        (re.compile(r"ImportError: No module named datetime\.foo;"
+                    " datetime is not a package"),
+         "ImportError: No module named foo"),
+        # PyPy 1.9 has some different error messages and reprs
+        (re.compile(r"ImportError: No module named datetime\.foo"),
+         "ImportError: No module named foo"),
+        (re.compile(r"<bound method type\.now of <type 'datetime\.datetime'>>"),
+         "<built-in method now of type object at ...>"),
+    ])
     return unittest.TestSuite((
-        doctest.DocFileSuite(README, optionflags=FLAGS, module_relative=False),
+        doctest.DocFileSuite(README,
+                             checker=checker,
+                             optionflags=FLAGS,
+                             module_relative=False),
     ))
