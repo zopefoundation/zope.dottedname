66726aa0c0c4db523d49dfbe49e6bd3e1f4ae7f1
Michael Howitz <mh@gocept.com>
2023-03-27 11:30:07 +0200

    Back to development: 6.1

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def read(*rnames):
 
 
 setup(name="zope.dottedname",
-      version='6.0',
+      version='6.1.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.dev',
       description='Resolver for Python dotted names.',
